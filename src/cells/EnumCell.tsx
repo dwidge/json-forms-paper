8548795c2c90bfc3374c9ae8b0b068d5b92fcfd8
dwidge <dwidge+gh@gmail.com>
2024-10-21 14:48:56 +0200

    Add licenses

--- src/cells/EnumCell.tsx
+++ src/cells/EnumCell.tsx
@@ -1,3 +1,28 @@
+/*
+  The MIT License
+  
+  Copyright (c) 2017-2019 EclipseSource Munich
+  https://github.com/eclipsesource/jsonforms
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+
 import React, { useMemo } from "react";
 import { Menu, Button } from "react-native-paper";
 import {
@@ -15,7 +40,7 @@ import { i18nDefaults, withVanillaEnumCellProps } from "../util/index.js";
 import type { VanillaRendererProps } from "../index.js";
 
 export const EnumCell = (
-  props: EnumCellProps & VanillaRendererProps & TranslateProps
+  props: EnumCellProps & VanillaRendererProps & TranslateProps,
 ) => {
   const {
     data,
@@ -30,7 +55,7 @@ export const EnumCell = (
   } = props;
   const noneOptionLabel = useMemo(
     () => t("enum.none", i18nDefaults["enum.none"], { schema, uischema, path }),
-    [t, schema, uischema, path]
+    [t, schema, uischema, path],
   );
 
   const [visible, setVisible] = React.useState(false);
@@ -62,5 +87,5 @@ export const EnumCell = (
 export const enumCellTester: RankedTester = rankWith(2, isEnumControl);
 
 export default withJsonFormsEnumCellProps(
-  withTranslateProps(withVanillaEnumCellProps(EnumCell))
+  withTranslateProps(withVanillaEnumCellProps(EnumCell)),
 ) as typeof EnumCell;
